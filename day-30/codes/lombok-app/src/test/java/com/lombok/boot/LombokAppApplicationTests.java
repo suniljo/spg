d339e1274dc9cbedd864b51fc577fda36ef177b2
Sunil Joseph <sunilozhakal@gmail.com>
2025-11-04 01:28:46 +0530

    day-30

---
+++ day-30/codes/lombok-app/src/test/java/com/lombok/boot/LombokAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lombok.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
